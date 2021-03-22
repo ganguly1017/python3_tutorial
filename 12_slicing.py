bb3ee6edcec28af449377c3049bbb9c86833c807
Sandeep Ganguly <sandeep@example.com>
2021-03-22 18:02:10 +0530

    slicing in python

---
+++ 12_slicing.py
@@ -0,0 +1,12 @@
+cities = ("Kanpur", "delhi", "noida", "lucknow", "lahore", "banglore")
+print(cities)
+
+# Slicing start
+subCity = cities[:-1]
+
+# "12" = "321"
+
+txt = "121"
+print(txt == txt[::-1])
+
+print(subCity)
