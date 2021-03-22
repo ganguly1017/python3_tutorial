789701604d4c2016f9a7744470c5a7ad73cb3cc5
Sandeep Ganguly <sandeep@example.com>
2021-03-22 17:06:02 +0530

    string program

---
+++ 07_string.py
@@ -0,0 +1,19 @@
+name = "sandeep"
+city = 'kanpur'
+
+multiString = ''' hkjhdkhjkhjkfkhjkds
+ jakdjdklljfkldjklfskldfjklsdf
+ jkfjksfhkdhfkhkjfhsfjkhjksfs
+ jkfjdkfhsjkdfhksjhfkshkfhskdjfjks
+ '''
+
+# string is collection of character
+
+#print("Name: {}, City: {}".format(name, city))
+#print(len(multiString))
+#print(multiString)
+
+para = "hkjd hjkdhak djkhdjkahdjk sakdhkjhjkahdkjsa"
+#print("hkjd" not in para)
+
+print(name[2])
