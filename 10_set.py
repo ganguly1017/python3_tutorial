22bf7788d05c56ecec84c4142e1ba13a2ca90818
Sandeep Ganguly <sandeep@example.com>
2021-03-22 17:41:21 +0530

    set in python

---
+++ 10_set.py
@@ -0,0 +1,15 @@
+names = { "sandeep", "amit", "abdul", "anish", "rashid" }
+
+# add value in set
+names.add("himanshi")
+
+# delte value from set
+names.discard("sandeep")
+
+print(names)
+
+skills = set(( "PHP", "Python", "Java", "C/C++" ))
+mixedSet = set(( 10, 10.56, False, "dkjhjkkj", 154.65 ))
+
+print(skills)
+print(mixedSet)
