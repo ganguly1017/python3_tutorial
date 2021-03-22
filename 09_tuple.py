026e6c9083e1c4ba7efcf98bd0a6bcdc1b58de00
Sandeep Ganguly <sandeep@example.com>
2021-03-22 17:31:31 +0530

    tuple in python

---
+++ 09_tuple.py
@@ -0,0 +1,18 @@
+
+# tuple is immutable => unchageable
+cities = ( "lahore", "kanpur", "delhi", "dhaka", "islamabad" )
+
+print(cities)
+
+names = tuple(("sandeep", "shoaib", "amit", "rajesh", "abdul"))
+
+print(names)
+
+mixedTuple = ( "dsfd", 454, 235.6, True )
+
+print(mixedTuple)
+
+# 2 X 2 tuple
+matTuple = ( ( 10, 20 ), (30, 40) )
+
+print(matTuple[1][1])
