689359078e51141b16aacf1b3a5ec3361ff48e30
Sandeep Ganguly <sandeep@example.com>
2021-03-22 17:21:41 +0530

    list in python

---
+++ 08_list.py
@@ -0,0 +1,18 @@
+# list is mutable => changeable
+cities = [ "kanpur", "kanpur", "karachi", "dhaka", "lucknow"  ]
+
+print(cities)
+
+cities.append("noida")
+
+print(cities[2])
+
+mixedList = list(( "dsfd", 4545, 2.6, True, "dsdsf", False ))
+
+print(mixedList)
+
+# 2 X 2
+
+mat = [ [10, 20], [30, 40] ]
+
+print(mat[1][1])
