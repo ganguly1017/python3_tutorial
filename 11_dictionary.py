863c135a903462f1e10883d39c8706877c5a83ca
Sandeep Ganguly <sandeep@example.com>
2021-03-22 17:52:34 +0530

    dictionary in python

---
+++ 11_dictionary.py
@@ -0,0 +1,16 @@
+product = {
+    "name": "Apple Phone",
+    "color": "Black",
+    "ram": "8GB",
+    "price": 250000,
+    "model_no": "XAP206",
+    "seller": ["Amazon", "Flipkart", "PayTM Mall", "Local Store"],
+    "isAvailable": True,
+    "cities": {
+        "india": "Delhi",
+        "pakistan": "Karachi",
+        "bangladesh": "Dhaka"
+    }
+}
+
+print(len(product))
