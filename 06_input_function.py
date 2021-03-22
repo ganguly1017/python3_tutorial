0c7546e2d0401943c23eb1252ad6ef8a72671e55
Sandeep Ganguly <sandeep@example.com>
2021-03-22 16:49:51 +0530

    input function detail

---
+++ 06_input_function.py
@@ -0,0 +1,22 @@
+# name = input("Enter your name: ")
+# salary = float(input("Enter your salary: "))
+#
+# a = 40
+# b = str(a)
+#
+# print("Hello {}".format(name))
+# print("Salary:  {}".format(salary))
+#
+# print("Salary Type: {}".format(type(salary)))
+# print("B Type:  {}".format(type(b)))
+
+def add():
+    a = int(input("Enter a : "))
+    b = int(input("Enter b : "))
+    return ( a + b )
+
+
+print("A + B = {}".format(add()))
+
+#input()
+
